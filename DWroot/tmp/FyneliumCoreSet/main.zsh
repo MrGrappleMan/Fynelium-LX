0bea84915111fe25004df0b901e2175706adebd8
Avaneesh <90239357+MrGrappleMan@users.noreply.github.com>
2025-08-30 21:57:05 +0530

    Update main.zsh

--- DWroot/tmp/FyneliumCoreSet/main.zsh
+++ DWroot/tmp/FyneliumCoreSet/main.zsh
@@ -1,17 +1,21 @@
-defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture ""
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-defaults write com.apple.frameworks.diskimages skip-verify -bool false
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool false
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool false
+variable dfwrt = "defaults write"
+
+dfwrt /Library/Preferences/com.apple.loginwindow DesktopPicture ""
+dfwrt com.apple.LaunchServices LSQuarantine -bool false
+dfwrt com.apple.frameworks.diskimages skip-verify -bool false
+dfwrt com.apple.frameworks.diskimages skip-verify-locked -bool false
+dfwrt com.apple.frameworks.diskimages skip-verify-remote -bool false
+dfwrt -g NSAutomaticSpellingCorrectionEnabled -bool true
+dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+dfwrt NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+dfwrt com.apple.Safari HomePage -string "https://scidsg.github.io/relaylove/"
+dfwrt com.apple.Finder ShowHardDrivesOnDesktop -bool true
+dfwrt com.apple.Finder ShowExternalHardDrivesOnDesktop -bool true
+dfwrt com.apple.Finder ShowRemovableMediaOnDesktop -bool true
+dfwrt com.apple.Finder ShowMountedServersOnDesktop -bool true
+dfwrt com.apple.Finder WarnOnEmptyTrash -bool true
+dfwrt com.apple.finder "FXEnableExtensionChangeWarning" -bool false
+dfwrt /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool NO
 tmutil enable
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool false
 brew analytics on
-defaults write -g NSAutomaticSpellingCorrectionEnabled -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-defaults write com.apple.Safari HomePage -string "https://scidsg.github.io/relaylove/"
-defaults write com.apple.Finder ShowHardDrivesOnDesktop -bool true
-defaults write com.apple.Finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.Finder ShowRemovableMediaOnDesktop -bool true
-defaults write com.apple.Finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.Finder WarnOnEmptyTrash -bool true
+pmset -b sleep 2
