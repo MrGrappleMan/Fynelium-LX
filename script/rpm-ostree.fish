78197b00516355e6442755758df112f740f4507b
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-14 22:05:29 +0530

    Update rpm-ostree.fish

--- script/rpm-ostree.fish
+++ script/rpm-ostree.fish
@@ -41,7 +41,7 @@ brh rebase testing -y
 
 # PKG ADD
    rotpkgadd "rust-zram-generator-devel preload \
-    tlp tlp-rdw \
+    tlp tlp-rdw \ 
     kernel-modules-extra uutils-coreutils util-linux \
     boinc-client boinc-client-static \
     flatseal flatpak-selinux flatpak-session-helper xdg-desktop-portal flatpak-libs libportal host-spawn \
@@ -54,7 +54,7 @@ brh rebase testing -y
     docker-cli docker-compose docker-buildx bottles \
     cosmic-epoch cosmic-desktop xdg-desktop-portal-cosmic initial-setup-gui-wayland-cosmic cosmic-greeter cosmic-comp cosmic-app-library cosmic-applets cosmic-edit cosmic-idle cosmic-osd cosmic-session cosmic-settings cosmic-settings-daemon cosmic-store fedora-release-cosmic-atomic cosmic-config-fedora greetd \
     openssh openssh-server mosh tor"
-    
+
     ## System Boosters ##
     ## Power management ##
     ## User environment ##
@@ -85,3 +85,43 @@ rpm-ostree kargs \
 
 # Universal InitRAMFS, reliable and standardized system
 rpm-ostree initramfs --disable
+
+    ## GhosTTY ## ghostty-nightly ghostty-nightly-fish-completion ghostty-nightly-shell-integration
+
+    ### Developer Specific:-
+    ### Suggested, use these in a container
+     ## Version Control Systems:
+      # git gh
+     ## Rust:
+      # rust cargo clippy
+     ## C stuff:
+      # cpp
+     ## Java:
+      # java-latest-openjdk
+     ## Python:
+      # 
+     ## Compilation:
+      # distcc distcc-server gcc gcc-c++
+     ## Penetration testing / Hacking:
+      # aircrack-ng turbo-attack golang-github-redteampentesting-monsoon
+     ## Artificial Intelligence:
+      # ollama
+
+    ### Gaming:-
+     ## Steam:
+      # steam steam-devices
+     ## Vavoom:
+      # vavoom vavoom-engine
+
+    ### Graphics:-
+     ## Mesa:
+      # mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld mesa-vulkan-drivers-freeworld
+      # mesa-dri-drivers
+      # mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers
+      # mesa-libOSMesa mesa-compat-libOSMesa
+     ## AMD: amd-gpu-firmware amd-ucode-firmware amdsmi am-utils
+     ## Nvidia: nvidia-gpu-firmware libva-nvidia-driver envytools nvidia-patch
+
+   ### Multipurpose:-
+    ## PKGMGR Snap
+     # snapd snapd-selinux
