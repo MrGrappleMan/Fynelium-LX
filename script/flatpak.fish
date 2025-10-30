348ca35c9243458377dba7af9a1fea6401a83245
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-30 11:43:27 +0530

    Logical commands

--- script/flatpak.fish
+++ script/flatpak.fish
@@ -2,38 +2,39 @@
 
 # 📛 Alias
 alias fpk "flatpak --system"
-alias fpkrepadd "flatpak --system remote-add --if-not-exists"
-alias fpkrepdel "flatpak --system remote-delete --force"
-alias fpkpkgadd "flatpak --system install -y --noninteractive --or-update"
-alias fpkpkgdel "flatpak --system uninstall -y --noninteractive --force-remove"
+alias fpkUpd "fpk update -y"
+alias fpkRep+ "fpk remote-add --if-not-exists"
+alias fpkRep- "fpk remote-delete --force"
+alias fpkPkg+ "fpk install -y --noninteractive --or-update"
+alias fpkPkg- "fpk uninstall -y --noninteractive --force-remove"
 
-# REP ADD
-  fpkrepadd flathub https://flathub.org/repo/flathub.flatpakrepo
-  fpkrepadd flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-  ###fpkrepadd eos-sdk https://ostree.endlessm.com/ostree/eos-sdk
-  fpkrepadd igalia https://software.igalia.com/flatpak-refs/igalia.flatpakrepo
-  fpkrepadd dragon-nightly https://cdn.kde.org/flatpak/dragon-nightly/dragon-nightly.flatpakrepo
-  ###fpkrepadd eos-apps https://ostree.endlessm.com/ostree/eos-apps
-  fpkrepadd webkit https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
-  fpkrepadd gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
-  fpkrepadd webkit-sdk https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
-  fpkrepadd fedora oci+https://registry.fedoraproject.org
-  fpkrepadd fedora-testing oci+https://registry.fedoraproject.org/#testing
-  ###fpkrepadd rhel https://flatpaks.redhat.io/rhel.flatpakrepo
-  fpkrepadd eclipse-nightly https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse.flatpakrepo
-  fpkrepadd elementaryos https://flatpak.elementary.io/repo.flatpakrepo
-  fpkrepadd pureos https://store.puri.sm/repo/stable/pureos.flatpakrepo
-  fpkrepadd kde-runtime-nightly https://cdn.kde.org/flatpak/kde-runtime-nightly/kde-runtime-nightly.flatpakrepo
+# REP +
+  fpkRep+ flathub https://flathub.org/repo/flathub.flatpakrepo
+  fpkRep+ flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+  ###fpkRep+ eos-sdk https://ostree.endlessm.com/ostree/eos-sdk
+  fpkRep+ igalia https://software.igalia.com/flatpak-refs/igalia.flatpakrepo
+  fpkRep+ dragon-nightly https://cdn.kde.org/flatpak/dragon-nightly/dragon-nightly.flatpakrepo
+  ###fpkRep+ eos-apps https://ostree.endlessm.com/ostree/eos-apps
+  fpkRep+ webkit https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
+  fpkRep+ gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+  fpkRep+ webkit-sdk https://software.igalia.com/flatpak-refs/webkit-sdk.flatpakrepo
+  fpkRep+ fedora oci+https://registry.fedoraproject.org
+  fpkRep+ fedora-testing oci+https://registry.fedoraproject.org/#testing
+  ###fpkRep+ rhel https://flatpaks.redhat.io/rhel.flatpakrepo
+  fpkRep+ eclipse-nightly https://download.eclipse.org/linuxtools/flatpak-I-builds/eclipse.flatpakrepo
+  fpkRep+ elementaryos https://flatpak.elementary.io/repo.flatpakrepo
+  fpkRep+ pureos https://store.puri.sm/repo/stable/pureos.flatpakrepo
+  fpkRep+ kde-runtime-nightly https://cdn.kde.org/flatpak/kde-runtime-nightly/kde-runtime-nightly.flatpakrepo
 
-# PKG DEL ## flatpaks should only be installed system wide, as they can share binaries between users, while keeping data separate for each. It also stnadardizes locations.
+# PKG - ## Flatpaks system wide, binaries shared between users, while kept data separate. It standardizes location and save storage.
   flatpak uninstall -u --all -y --noninteractive --force-remove
 
-# PKG ADD
-  ##fpkpkgadd flathub-beta \
+# PKG +
+  ##fpkPkg+ flathub-beta \
    ##org.freedesktop.Platform org.gnome.Platform \
    ##org.freedesktop.Sdk org.gnome.Sdk
 
-  fpkpkgadd flathub \
+  fpkPkg+ flathub \
    com.rafaelmardojai.Blanket \
    io.github.celluloid_player.Celluloid \
    io.github.flattool.Warehouse \
@@ -46,11 +47,11 @@ alias fpkpkgdel "flatpak --system uninstall -y --noninteractive --force-remove"
    org.torproject.torbrowser-launcher \
    io.github.ecotubehq.player
 
-  fpkpkgadd flathub-beta \
+  fpkPkg+ flathub-beta \
    com.visualstudio.code.insiders \
    io.mrarm.mcpelauncher \
    io.frama.tractor.carburetor \
    org.inkscape.Inkscape org.kde.krita
 
-  fpkpkgadd gnome-nightly \
+  fpkPkg+ gnome-nightly \
    org.gnome.BoxesDevel
