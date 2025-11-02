b314e0de982a5961b7d12937b9d76f636fd7e788
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-02 17:53:14 +0530

    Update flatpak.fish

--- script/flatpak.fish
+++ script/flatpak.fish
@@ -5,7 +5,43 @@ alias fpk "flatpak --system"
 alias fpkUpd "fpk update -y"
 alias fpkRep+ "fpk remote-add --if-not-exists"
 alias fpkRep- "fpk remote-delete --force"
-alias fpkPkg+ "fpk install -y --noninteractive --or-update"
+alias fpkPkg+Adv "fpk install -y --noninteractive --or-update"
+function fpkPkg+ -d "Flatpak install if present in remote"
+    set -l remote flathub  # Change if you use a different primary remote
+    set packages $argv
+    if test (count $argv) -eq 1 -a -n (string match '* *' $argv[1])
+        set packages (string split ' ' $argv[1])
+    end
+    set -l install_list
+    for pkg in $packages
+        # Search and parse output (tab-separated by default)
+        set -l output (flatpak search --columns=application,remotes $pkg $scope 2>/dev/null)
+        if test -z "$output"
+            continue  # No results
+        end
+        set -l lines (string split \n -- $output)
+        # Skip header if present
+        set -l found false
+        for line in $lines[2..-1]  # Assume first line is header
+            set -l fields (string split \t -- $line)
+            set -l candidate $fields[1]
+            set -l remotes $fields[2]
+            if test "$candidate" = "$pkg"
+                # Exact match; check if our remote is available
+                if string match -q "*$remote*" $remotes
+                    set found true
+                    break
+                end
+            end
+        end
+        if $found
+            set install_list $install_list $pkg
+        end
+    end
+    if test (count $install_list) -gt 0
+        flatpak install -y --noninteractive --or-update --system $remote $install_list
+    end
+end
 alias fpkPkg- "fpk uninstall -y --noninteractive --force-remove"
 
 # REP -
@@ -37,8 +73,7 @@ alias fpkPkg- "fpk uninstall -y --noninteractive --force-remove"
    ##org.freedesktop.Platform org.gnome.Platform \
    ##org.freedesktop.Sdk org.gnome.Sdk
 
-  fpkPkg+ flathub \
-    com.rafaelmardojai.Blanket \
+  fpkPkg+ "com.rafaelmardojai.Blanket \
     io.github.flattool.Warehouse \
     edu.berkeley.BOINC \
     org.vinegarhq.Sober io.mrarm.mcpelauncher \
@@ -50,7 +85,7 @@ alias fpkPkg- "fpk uninstall -y --noninteractive --force-remove"
     com.visualstudio.code \
     org.inkscape.Inkscape org.upscayl.Upscayl org.kde.krita org.blender.Blender xyz.woxel.Woxel com.obsproject.Studio \
     org.telegram.desktop io.github.tobagin.karere dev.vencord.Vesktop \
-    com.github.wwmm.easyeffects org.nickvision.cavalier
+    com.github.wwmm.easyeffects org.nickvision.cavalier"
 
     fpkPkg+ cosmic \
       io.github.cosmic_utils.cosmic-ext-applet-clipboard-manager \
