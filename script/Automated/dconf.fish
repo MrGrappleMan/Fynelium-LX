b3ddc52fdc3f7fc760d661a372e0f375daa8d60e
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-06 11:28:31 +0530

    Enhance comment for Dconf configuration script
    Expanded the comment to clarify the purpose of the script.

--- script/Automated/dconf.fish
+++ script/Automated/dconf.fish
@@ -1,8 +1,11 @@
 #!/usr/bin/env fish
 
-# 👣 Configures GSettings / Dconf
+# 👣 Configures GSettings / Dconf - The settings that affect the experience of GNOME
 
 # Gets executed once by root, then the user, as seeen in documentation.
 
+# Generic settings
 dconf load / < /tmp/Fynelium-LX/export/gnome.dconf
+
+# For specialized extensions
 dconf load /org/gnome/shell/extensions/ < /tmp/Fynelium-LX/export/gnome-extensions.dconf
