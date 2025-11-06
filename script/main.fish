40213dce55e5fea453c083bac0f2d500d91e7611
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-06 11:36:12 +0530

    Update script to handle root and user execution

--- script/main.fish
+++ script/main.fish
@@ -1,14 +1,14 @@
 #!/bin/fish
 
 if fish_is_root_user
-    echo "Running as root. Please run as intended in the documentation by directly copy-pasting the box"
-    echo "without root permissions"
+    echo "Running as root! Please run as intended in the documentation by directly copy-pasting the box"
+    echo "that is, without root permissions. Some scripts need to be executed as a normal user"
     exit
 else
-    echo "Running as a regular user. Proceeding..."
+    echo "Proceeding..."
 end
 
-sudo curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/bulkexec.fish" | sudo fish
-curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/ujust.fish" | fish
-curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/gsettings.fish" | fish
+sudo curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/Automated.fish" | sudo fish
+curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/Automated/ujust.fish" | fish
+curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/Automated/gsettings.fish" | fish
 systemctl reboot
