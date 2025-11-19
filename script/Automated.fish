b726eff4d44103733b931f28c72f2caf576763de
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-20 02:18:56 +0530

    Update Automated.fish

--- script/Automated.fish
+++ script/Automated.fish
@@ -1,34 +1,38 @@
 #!/usr/bin/env fish
 
+clear
 # RootCheck
 if fish_is_root_user
-    echo "Starting..."
+    echo "Is root user, proceeding..."
 else
-    echo "Not running as root! An internal error has happened. You are not supposed to run this by itself"
+    echo "Not running as root! You are supposed to run this as root"
     exit
 end
 
 # Retrieval of project files
-
+echo Getting project files from GitHub
 cd /tmp/
 rm -rf /tmp/Fynelium-LX
 mkdir -p /tmp/Fynelium-LX
 git clone https://github.com/MrGrappleMan/Fynelium-LX.git /tmp/Fynelium-LX/
 /tmp/Fynelium-LX/
 
-# Copy over to filesystem
+# FS Copy over
+echo Copying over additions to filesystem
 cp -r /tmp/Fynelium-LX/FSRoot/etc/* /etc/
 cp -r /tmp/Fynelium-LX/FSRoot/var/* /var/
 cp -r /tmp/Fynelium-LX/FSRoot/opt/* /opt/
 ##cp -r /tmp/Fynelium-LX/FSRoot/root/* /root/
 
-# Permissions
+# FS Perms
+echo Setup filesystem permissions
 mkdir -p /etc/playit
 mkdir -p /opt/playit
 chmod a+x /opt/playit/playit
 chmod a+x /opt/mc-server/mc-server
 
 # Subexecution of sub-scripts that dont require user interaction. doubtful of ujust and flatpak
+echo Now executing subscripts
 fish /tmp/Fynelium-LX/script/Automated/rpm-ostree.fish
 fish /tmp/Fynelium-LX/script/Automated/flatpak.fish
 fish /tmp/Fynelium-LX/script/Automated/systemd.fish
