a4eb2e1832e4d372cb98dba49778f864edd70c84
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-24 21:28:51 +0530

    Update flatpak.fish

--- script/Automated/flatpak.fish
+++ script/Automated/flatpak.fish
@@ -85,7 +85,6 @@ alias fpkPkg- "fpk uninstall -y --noninteractive --force-remove" # Package remov
     edu.berkeley.BOINC \
     org.vinegarhq.Sober io.mrarm.mcpelauncher app.twintaillauncher.ttl com.heroicgameslauncher.hgl \
     rocks.shy.VacuumTube com.warlordsoftwares.youtube-downloader-4ktube io.github.ecotubehq.player \
-    com.google.ChromeDev \
     com.ranfdev.DistroShelf org.gnome.Boxes rs.ruffle.Ruffle \
     io.github.brunofin.Cohesion \
     io.frama.tractor.carburetor \
