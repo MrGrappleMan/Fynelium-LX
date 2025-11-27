1f363d32184c27a8af1b25f3350657f32141f585
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-27 16:44:40 +0530

    Update sysfresh.fish

--- FSRoot/opt/Fyn-scripts/sysfresh.fish
+++ FSRoot/opt/Fyn-scripts/sysfresh.fish
@@ -1,3 +1,20 @@
 #!/usr/bin/env fish
 
 ### This file is for overall system maintenance with custom arguments, till a proper topgrade config has been made
+### Currently, a makeshift version of uupd
+
+# Systemd
+
+sysctl --system
+systemctl daemon-reload
+
+# RPM-OSTree
+
+rpm-ostree -q reload
+rpm-ostree -q upgrade --trigger-automatic-update-policy --allow-downgrade --bypass-driver
+rpm-ostree apply-live
+rpm-ostree apply-live --allow-replacement
+
+# Flatpak
+
+flatpak --system update -y --force-remove --appstream --noninteractive
