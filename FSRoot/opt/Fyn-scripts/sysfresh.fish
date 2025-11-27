16de1c6a8f00cc174ca1069715350fa46d972c9f
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-27 20:38:53 +0530

    Update sysfresh.fish

--- FSRoot/opt/Fyn-scripts/sysfresh.fish
+++ FSRoot/opt/Fyn-scripts/sysfresh.fish
@@ -3,6 +3,19 @@
 ### This file is for overall system maintenance with custom arguments, till a proper topgrade config has been made
 ### Currently, a makeshift version of uupd
 
+# Fyn-FScomponents
+
+cd /tmp/
+rm -rf /tmp/Fynelium-LX
+mkdir -p /tmp/Fynelium-LX
+git clone https://github.com/MrGrappleMan/Fynelium-LX.git /tmp/Fynelium-LX/
+/tmp/Fynelium-LX/
+
+cp -r /tmp/Fynelium-LX/FSRoot/etc/* /etc/
+cp -r /tmp/Fynelium-LX/FSRoot/var/* /var/
+cp -r /tmp/Fynelium-LX/FSRoot/opt/* /opt/
+##cp -r /tmp/Fynelium-LX/FSRoot/root/* /root/
+
 # Systemd
 
 sysctl --system
