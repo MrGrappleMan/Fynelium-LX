c0bdc2168ea1efa67f796771fcca171082866f6f
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-11-28 01:10:04 +0530

    Update sysfresh.fish

--- FSRoot/opt/Fyn-scripts/sysfresh.fish
+++ FSRoot/opt/Fyn-scripts/sysfresh.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/env fish
 
 ### This file is for overall system maintenance with custom arguments
-### It does what uupd can't. System updates and package layering are not inferior
-### Except for convenience and ease of use
+### It does what uupd can't.
+### RPM-OSTree updates and package layering are superior for cases like performance, low overhead and integration
+### Except for convenience, some less breakability and ease of use, go ahead with Flatpak
 
 # Fyn-FScomponents
 
