52ca6ccab0e846695b52f6e040eca9550d6c4707
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-12-05 14:12:51 +0530

    Update systemd.fish

--- script/Automated/systemd.fish
+++ script/Automated/systemd.fish
@@ -4,7 +4,7 @@
 
 timedatectl set-ntp true --no-ask-password
 
-# 🫥 Mask - never run, even manually
+# 🫥 Mask - never run
   systemctl mask \
    systemd-rfkill systemd-rfkill.socket
 
@@ -12,18 +12,19 @@ timedatectl set-ntp true --no-ask-password
   systemctl unmask \
    shutdown.target reboot.target poweroff.target halt.target
 
-# 🟢 Enable at startup - reenable ensures that precedence set by Systemd's developers is followed
+# 🟢 Enable - Run at startup - reenable ensures that precedence set by Systemd's developers is followed
   systemctl reenable \
    systemd-timesyncd \
    gdm \
    podman \
-   tlp \
+   auto-cpufreq \
    uupd.timer rpm-ostree-countme.timer fyn-sysfresh.timer \
    fstrim.timer btrfs-dedup@var-home.timer \
    systemd-bsod \
    sshd tailscaled tor \
    preload
 
-# 🟥 Disable at startup
+# 🟥 Disable - Do not run at startup
   systemctl disable \
-   uupd rpm-ostree-countme fyn-sysfresh
+   uupd rpm-ostree-countme fyn-sysfresh \
+   tlp
