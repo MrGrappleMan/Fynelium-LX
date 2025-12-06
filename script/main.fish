62f44016dd7f2bad393a45ec41e8b69da4039493
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-12-07 01:00:32 +0530

    Update main.fish

--- script/main.fish
+++ script/main.fish
@@ -2,14 +2,11 @@
 
 clear
 if fish_is_root_user
-    echo "Running as root! Please run as intended in the documentation by directly copy-pasting the box"
-    echo "that is, without root permissions. Some scripts need to run as regular user."
+    echo "Running as root! Please re-run as a regular user, as intended."
     exit
-else
-    echo "Regular user, proceeding..."
 end
 
-echo Insert the password for your user
+echo Insert the password for your user,
 sudo curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/Automated.fish" | sudo fish
 curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/Automated/ujust.fish" | fish
 curl "https://raw.githubusercontent.com/MrGrappleMan/Fynelium-LX/refs/heads/main/script/Automated/gsettings.fish" | fish
