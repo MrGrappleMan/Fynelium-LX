96835bcbbe74cf748122f24e4c1974952e3594fd
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-12-26 21:31:59 +0530

    Update Automated.fish

--- script/Automated.fish
+++ script/Automated.fish
@@ -33,14 +33,18 @@ chmod a+x /opt/playit/playit
 chmod a+x /opt/mc-server/mc-server
 chown -R boinc:boinc /var/lib/boinc/
 chmod -R 755 /var/lib/boinc/
+chmod 600 /var/lib/boinc/gui_rpc_auth.cfg
 
 # User perms
-usermod -aG boinc,video boinc
+usermod -aG video,render boinc
 usermod -aG boinc root
 usermod --add-subuids 100000-165535 --add-subgids 100000-165535 boinc
 
 # SELinux
 setsebool -P container_use_devices true
+
+# LoginCtl
+loginctl enable-linger boinc
 
 # Subexecution of sub-scripts that dont require user interaction. doubtful of ujust and flatpak
 echo Now executing subscripts
