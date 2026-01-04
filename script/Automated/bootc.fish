5729711e2518d594a713442d17869da10870593d
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-04 15:54:00 +0530

    Update bootc.fish

--- script/Automated/bootc.fish
+++ script/Automated/bootc.fish
@@ -1,15 +1,16 @@
 #!/bin/fish
 
 # ⛑️ Ensure clean state - remove compatibility
+rpm-ostree cancel -q
 rpm-ostree reset -l -o -i -q
-ostree admin pin --unpin 0
-ostree admin pin --unpin 1
-ostree admin pin --unpin 2
-ostree admin pin --unpin 3
-ostree admin pin --unpin 4
+ostree admin pin -u 0
+ostree admin pin -u 1
+ostree admin pin -u 2
+ostree admin pin -u 3
+ostree admin pin -u 4
 
 # 🎛️ Switch base - will change up later with custom image
-bootc switch ghcr.io/ublue-os/bazzite-dx-gnome:latest --soft-reboot auto --quiet
+bootc switch ghcr.io/ublue-os/bazzite-dx-gnome:latest --quiet
 
 # 📈 Upgrade
 bootc upgrade --quiet
