761f5ab644d9bdcd9e62e9d25b641146c61f6d1c
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-06 01:27:10 +0530

    Update dconf.fish

--- script/Automated/dconf.fish
+++ script/Automated/dconf.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 
-# 👣 Configures GSettings / Dconf - The settings that affect the experience of GNOME
+# 👣 Configures GSettings / Dconf - The settings for GNOME
 
 # Gets executed once by root, then the user, as seeen in documentation.
 
-# Generic settings
-dconf load / < /tmp/Fynelium-LX/export/gnome.dconf
+# Generic
+dconf load -f / < /tmp/Fynelium-LX/export/gnome.dconf
 
-# For specialized extensions
-dconf load /org/gnome/shell/extensions/ < /tmp/Fynelium-LX/export/gnome-extensions.dconf
+# For extensions
+dconf load -f /org/gnome/shell/extensions/ < /tmp/Fynelium-LX/export/gnome-extensions.dconf
