7cd6b493dce3334ffed63e9d060d9a4ec7c16cfc
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-01-08 14:54:48 +0530

    Update sysfresh.fish

--- FSRoot/opt/fyn-scripts/sysfresh.fish
+++ FSRoot/opt/fyn-scripts/sysfresh.fish
@@ -1,22 +1,16 @@
 #!/usr/bin/env fish
 
-### This file is for overall system maintenance with custom arguments
-### It does what uupd can't.
-### RPM-OSTree updates and package layering are superior for cases like performance, low overhead and integration
-### Except for convenience, some less breakability and ease of use, go ahead with Flatpak
+### Just copies over filesystem components
 
-# Fyn-FScomponents
+# Get
 cd /tmp/
 rm -rf /tmp/Fynelium-LX
 mkdir -p /tmp/Fynelium-LX
 git clone https://github.com/MrGrappleMan/Fynelium-LX.git /tmp/Fynelium-LX/
 cd /tmp/Fynelium-LX/
 
+# Copy-over
 cp -r /tmp/Fynelium-LX/FSRoot/etc/* /etc/
 cp -r /tmp/Fynelium-LX/FSRoot/var/* /var/
 cp -r /tmp/Fynelium-LX/FSRoot/opt/* /opt/
 ##cp -r /tmp/Fynelium-LX/FSRoot/root/* /root/
-
-# Systemd
-sysctl --system
-systemctl daemon-reload
